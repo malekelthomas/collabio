a7faac782fd7ead3b4ca407c95f95c93b9e0e6d4
maleke <malekelthomas97@gmail.com>
2020-10-23 13:28:34 -0400

    imported users route

--- api/server.js
+++ api/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
 require('dotenv/config');
 
+//import routes
+const usersRoute = require('./routes/users');
+
+app.use('/users', usersRoute);
+
+//connect to db
 mongoose.connect(process.env.MONGO_CONNECTION, {useUnifiedTopology:true, useNewUrlParser:true}, () => {
     console.log('Connected to DB!');
 });
 
+//listening on server
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
