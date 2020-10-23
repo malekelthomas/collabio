749117d46a10cdb70761bc08eeda130d75928760
maleke <malekelthomas97@gmail.com>
2020-10-23 13:38:21 -0400

    added user schema

---
+++ api/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = mongoose.Schema({
+    user_name: {
+        type:String,
+        required: true
+        },
+    first_name: {
+        type:String,
+        required: true
+        },
+    last_name: {
+        type:String,
+        required: true
+        },
+    password: {
+        type:String,
+        required: true
+        },
+});
+
+module.exports = mongoose.model('User', UserSchema);
