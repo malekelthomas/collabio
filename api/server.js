3eccb68850764f5b148cd4553d01518b92f0264c
maleke <malekelthomas97@gmail.com>
2020-10-24 04:47:25 -0400

    added cors

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config');
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 
 //import routes
