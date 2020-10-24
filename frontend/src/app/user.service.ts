86c6e11fed3bee54eb62bd5bc1624000545e09ab
maleke <malekelthomas97@gmail.com>
2020-10-24 04:45:53 -0400

    added getUsers()

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { catchError, tap, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  private usersUrl = 'api/users';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  };
+
+  getUsers(): Observable<Object> {
+
+      return this.http.get(this.usersUrl, this.httpOptions);
+
+  }
+
 }
--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service'
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getUsers();
   }
 
+  users;
+
+  getUsers(): void {
+    this.userService.getUsers()
+      .subscribe(users => this.users = users)
+  }
 }
