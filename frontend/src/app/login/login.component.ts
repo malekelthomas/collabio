1dfea37d8c934dcbd377223fdc953a31bbe5c646
maleke <malekelthomas97@gmail.com>
2020-10-26 14:05:51 -0400

    Add methods to handle login form

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, EventEmitter, Component, OnInit, Output } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { LoginService } from '../login.service';
+
 
 @Component({
   selector: 'app-login',
@@ -9,11 +11,25 @@ import { LoginService } from '../login.service';
 export class LoginComponent implements OnInit {
 
   loggedIn;
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
+
   constructor(private loginS: LoginService) { }
 
   ngOnInit(): void {
+    this.submit();
   }
 
+  @Output() submitEm: EventEmitter<unknown> = new EventEmitter;
+  submit() {
+    if (this.form.valid) {
+      this.submitEm.emit(this.form.value);
+      console.log(this.form.value)
+    }
+  }
 
 
 }
