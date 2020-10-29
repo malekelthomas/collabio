364c8cd618e934055ef6f13198eeb80250dc733a
maleke <malekelthomas97@gmail.com>
2020-10-29 04:13:47 -0400

    added register method

--- frontend/src/app/register.service.ts
+++ frontend/src/app/register.service.ts
@@ -1,9 +1,24 @@
+import { User } from './user';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { catchError, tap, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': localStorage.getItem('token')})
+  };
+
+  private registerUrl = 'api/register'
+
+  register(credentials: User): Observable<User>{
+    console.log(credentials)
+    return this.http.post<User>(`${this.registerUrl}`, credentials);
+  }
 }
