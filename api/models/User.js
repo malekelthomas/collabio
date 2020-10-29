dbd47846cf097d2830a252fe6bb42db793c56878
maleke <malekelthomas97@gmail.com>
2020-10-29 01:35:55 -0400

    added pre save hash and compare passwords method

--- api/models/User.js
+++ api/models/User.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 
 const UserSchema = mongoose.Schema({
     user_name: {
+        type:String,
+        required: true,
+        unique: true,
+        },
+    email: {
         type:String,
         required: true,
         unique: true,
@@ -26,5 +31,29 @@ const UserSchema = mongoose.Schema({
         type: Array 
     }
 });
+
+UserSchema.pre('save', function(next){ // before saving user model
+    let user = this;
+    if(!user.isModified('password')){
+        return next();
+    }
+
+    bcrypt.hash(user.password, 10, (err, hash) => {
+        if(err){
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+});
+
+UserSchema.methods.comparePassword = (passwordEntered, cb) => {
+    bcrypt.compare(passwordEntered, this.password, (err, isMatch) => {
+        if(err){
+            return cb(err);
+        }
+        cb(null, isMatch)
+    })
+};
 
 module.exports = mongoose.model('User', UserSchema);
