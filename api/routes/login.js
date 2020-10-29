a609f34bc97c7939e1f1421db18fceb804baa0c0
maleke <malekelthomas97@gmail.com>
2020-10-29 04:15:55 -0400

    replaced compare method wtih model method

--- api/routes/login.js
+++ api/routes/login.js
@@ -12,18 +12,32 @@ require('dotenv/config');
 router.post('/', async (req, res) => {
     try{
         const user = await User.findOne({"email":req.body.email})
-        let match = bcrypt.compare(user.password, req.body.password)
-        if (match){
-            jwt.sign({user:user},process.env.SECRET_KEY, (err, token) => {
-                res.json({
-                    token
-                });
-            })
+        user.comparePassword(req.body.password, function(err, isMatch){
+            if(isMatch){
+                //res.json(user)
+                jwt.sign({user:user},process.env.SECRET_KEY, (err, token) => {
+                    res.json({
+                        token
+                    });
+                })
+            }
+            else{
+                res.json({message:"Login details incorrect."})
+            }
+            
+        })
+    }
+    catch (err){
+        res.json({message:"Login details incorrect."})
+        }
+       
+    /*     if (match){
+            
         }
     } catch (err) {
         res.json({message:err});
     }
-
+ */
 })
 
 module.exports = router;
