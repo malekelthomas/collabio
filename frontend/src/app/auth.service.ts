d053f572cb784fe95218cf8b738daa0d1fcb957c
maleke <malekelthomas97@gmail.com>
2020-10-30 03:55:55 -0400

    moved login and register into authservice

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -7,12 +7,13 @@ import { catchError, tap, map} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class LoginService {
+export class AuthService {
   constructor(private http: HttpClient) { }
 
   private token;
   private usersUrl = 'api/users';
   private loginUrl = 'api/login'
+  private registerUrl = 'api/register'
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': localStorage.getItem('token')})
@@ -22,5 +23,15 @@ export class LoginService {
     this.http.post<User>(`${this.loginUrl}`, {email:credentials.email, password:credentials.password})
       .subscribe(data => localStorage.setItem('token', data.token));
     return this.http.post<User>(`${this.loginUrl}`, {email:credentials.email, password:credentials.password});
+  }
+
+
+  register(credentials: User): Observable<User>{
+    console.log(credentials)
+    return this.http.post<User>(`${this.registerUrl}`, credentials);
+  }
+
+  loggedIn(){
+    return !!localStorage.getItem('token');
   }
 }
