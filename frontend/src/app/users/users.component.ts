f50332bfbaf7c736ff1e065b4ea8c79160dbc5ce
maleke <malekelthomas97@gmail.com>
2020-10-30 04:08:50 -0400

    updated to redirect if server responds with error

--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service'
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-users',
@@ -13,23 +14,38 @@ export class UsersComponent implements OnInit {
   constructor(
     private userService: UserService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private router: Router) { }
 
   ngOnInit(): void {
-    this.getUsers();
+    this.getUser();
     this.getFollowers();
   }
 
   users;
   followers;
 
-  getUsers(): void {
+  checkError(error: HttpErrorResponse): boolean{
+    if (error instanceof HttpErrorResponse){
+      this.router.navigate(['/login']);
+      return true;
+    }
+    else{
+      console.error(error);
+      return false;
+    }
+
+  }
+  getUser(): void {
     this.userService.getUsers()
-      .subscribe(users => this.users = users)
+      .subscribe(
+        users => this.users = users,
+        error => this.checkError(error)
+        )
   }
 
   getFollowers(): void {
     this.userService.getFollowers()
-      .subscribe(followers => this.followers = followers)
+      .subscribe(followers => this.followers = followers, error => this.checkError(error))
   }
 }
