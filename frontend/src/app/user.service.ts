bcb38380b55bc855190900f439a9c75c2e1e6524
maleke <malekelthomas97@gmail.com>
2020-10-31 07:11:04 -0400

    emits loggedin users name and when logged out

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, Output, EventEmitter } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { catchError, tap, map} from 'rxjs/operators';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  private user;
   constructor(private http: HttpClient) { }
 
 
@@ -15,18 +17,29 @@ export class UserService {
   private followersUrl = '/followers';
   private currentUser = 'maleke';
 
+  @Output() loggedInName: EventEmitter<any> = new EventEmitter();
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': `Bearer ${localStorage.getItem('token')}`}),
   };
 
-  getUsers(): Observable<Object> {
-
-      return this.http.get(this.usersUrl, this.httpOptions);
+  getUser(): Observable<User> {
+      if(localStorage.getItem('email')){
+        this.http.get<User>(`${this.usersUrl}/${localStorage.getItem('email')}`, this.httpOptions)
+        .subscribe(user => {
+          this.loggedInName.emit(user.user_name)
+        })
+        return this.http.get<User>(`${this.usersUrl}/${localStorage.getItem('email')}`, this.httpOptions);
+      }
   }
 
   getFollowers(): Observable<Object> {
 
     return this.http.get(`${this.usersUrl}/${this.currentUser}/${this.followersUrl}`);
+  }
+
+
+  logout(){
+    this.loggedInName.emit('logged out');
   }
 
 }
