b9e6642acaa0b849b503cc82cdfca12d495e48ec
maleke <malekelthomas97@gmail.com>
2020-11-01 01:06:03 -0500

    added function to close search, navigate to user

--- frontend/src/app/user-search/user-search.component.ts
+++ frontend/src/app/user-search/user-search.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { Observable, Subject } from 'rxjs/';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { User } from '../user';
 import { UserService } from './../user.service';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-user-search',
@@ -14,7 +16,21 @@ export class UserSearchComponent implements OnInit {
   users$: Observable<User[]>;
   private searchTerms = new Subject<string>();
 
-  constructor(private userService: UserService) { }
+  @ViewChild('searchList') searchList: ElementRef;
+  @ViewChild('searchBox') searchBox: ElementRef;
+  constructor(private userService: UserService, private router: Router, private renderer: Renderer2) {
+
+    this.renderer.listen('window', 'click', (e:Event) =>{ //monitoring clicks on the page
+
+      if(e.target !== this.searchList.nativeElement){
+        this.searchTerms.next(""); //set to null
+        this.searchBox.nativeElement.value = ""; //clear input
+
+      }
+    })
+
+
+   }
 
   search(term: string): void {
     this.searchTerms.next(term);
@@ -31,6 +47,11 @@ export class UserSearchComponent implements OnInit {
 
       switchMap((term: string) => this.userService.searchUsers(term)),
     );
+  }
+
+
+  goToUser(user: string){
+    this.router.navigate(['/users'], {queryParams:{user_name:user}});
   }
 
 }
