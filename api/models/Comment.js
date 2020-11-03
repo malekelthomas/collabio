8c0408ed1fcb8fcc144ae83cd1be83ecb7fedf9b
maleke <malekelthomas97@gmail.com>
2020-11-03 01:33:40 -0500

    updated schema

--- api/models/Comment.js
+++ api/models/Comment.js
@@ -1,22 +1,39 @@
 const mongoose = require('mongoose');
 
 
+const baseCommentSchema = mongoose.Schema({
+    author: {
+        type: String,
+        required: true
+    },
+    timePosted: {
+        type: Date,
+        default: Date.now
+    },
+    comment:String,
+    likes: Number,
+});
+
 const CommentSchema = mongoose.Schema({
     author: {
         type: String,
         required: true
     },
     replies: {
-        type:[CommentSchema],
-        default: undefined
+        type:[baseCommentSchema],
     },
     timePosted: {
         type: Date,
         default: Date.now
     },
+    comment: String,
     likes: Number,
 });
 
 
-
-module.exports = mongoose.model('Comment', CommentSchema);
+const InitComment = mongoose.model('Comment', CommentSchema)
+const ReplyComment = mongoose.model('ReplyComment', baseCommentSchema);
+module.exports = {
+    InitComment,
+    ReplyComment
+}
