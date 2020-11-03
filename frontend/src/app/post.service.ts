92703e63022561911a24b7722c59a52d2070f6e7
maleke <malekelthomas97@gmail.com>
2020-11-03 03:44:02 -0500

    post service

--- frontend/src/app/post.service.ts
+++ frontend/src/app/post.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Output, EventEmitter } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { catchError, tap, map} from 'rxjs/operators';
+import { ReplyComment } from './replyComment';
+import { InitComment } from './comment';
+import { Post } from './post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private usersUrl = 'api/users';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': `Bearer ${localStorage.getItem('token')}`}),
+  };
+
+  getPosts(): Observable<Post>{
+    return this.http.get<Post>(`${this.usersUrl}/user/${localStorage.getItem('user_name')}/posts`)
+  }
+
 }
