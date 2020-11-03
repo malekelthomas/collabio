c80cbf57ed80a62c0ebc30e4257fd7083b1a4922
maleke <malekelthomas97@gmail.com>
2020-11-03 03:43:47 -0500

    post component

--- frontend/src/app/post/post.component.ts
+++ frontend/src/app/post/post.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ReplyComment } from '../replyComment';
+import { InitComment } from '../comment';
+import { PostService } from './../post.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
+
+
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  posts;
+  comments;
+  loadedComments;
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.getPosts();
+  }
+
+
+
+
+  getPosts(){
+    //
+    this.postService.getPosts().subscribe(posts => {
+      this.posts = posts;
+      console.log(this.posts)
+    });
+  }
+
+  loadComments(){
+    this.posts.forEach(post => {
+
+      post.comments.forEach(comment =>{
+        this.comments = comment;
+        this.loadedComments = true;
+        console.log(this.comments);
+      })
+  })
+
   }
 
 }
